4f26c53a47fd10c6c1e0efa7c03bdb0485e7fc85
Stanislav Rodionov <s.rodionov@vizerra.com>
2011-12-02 23:12:46 +0400

    FSLM: particle vs static body collision in progress

--- Research/Physics/FastLatticeShapeMatching/Source/Testbed/Interactions/Impulses/BodyImpulse.cs
+++ Research/Physics/FastLatticeShapeMatching/Source/Testbed/Interactions/Impulses/BodyImpulse.cs
@@ -13,27 +13,47 @@ namespace PhysicsTestbed
             this.body = body;
         }
 
+        public bool CollideSweptSegments(LineSegment first, LineSegment second, ref Vector2 intersection)
+        {
+            // TODO: test this algorithm and optimize if needed
+            double Ua, Ub;
+            // Equations to determine whether lines intersect
+            Ua = ((second.end.X - second.start.X) * (first.start.Y - second.start.Y) - (second.end.Y - second.start.Y) * (first.start.X - second.start.X)) /
+                ((second.end.Y - second.start.Y) * (first.end.X - first.start.X) - (second.end.X - second.start.X) * (first.end.Y - first.start.Y));
+            Ub = ((first.end.X - first.start.X) * (first.start.Y - second.start.Y) - (first.end.Y - first.start.Y) * (first.start.X - second.start.X)) /
+                ((second.end.Y - second.start.Y) * (first.end.X - first.start.X) - (second.end.X - second.start.X) * (first.end.Y - first.start.Y));
+            if (Ua >= 0.0f && Ua <= 1.0f && Ub >= 0.0f && Ub <= 1.0f)
+            {
+                intersection.X = first.start.X + Ua*(first.end.X - first.start.X);
+                intersection.Y = first.start.Y + Ua*(first.end.Y - first.start.Y);
+                return true;
+            }
+            return false;
+        }
+
         public override void ApplyImpulse(System.Collections.IEnumerable particles)
         {
             // avoid self-collision
             if (particles.Equals(body.particles))
                 return;
 
-            bool collisionDetected = false;
             foreach (Particle t in particles)
             {
-                foreach (Particle bt in body.particles)
+                foreach (Particle bodyt in body.particles)
                 {
-                    if ((t.x - bt.x).Length() < 10.0)
+                    Vector2 intersection = new Vector2();
+                    if (bodyt.xPos != null)
                     {
-                        collisionDetected = true;
-                        //Testbed.PostMessage(System.Drawing.Color.Green, "Collision detected!"); // DEBUG
+                        if (CollideSweptSegments(new LineSegment(bodyt.goal, bodyt.xPos.goal), new LineSegment(t.x, t.x + t.v), ref intersection))
+                            Testbed.PostMessage(System.Drawing.Color.Green, "LineSegments Collision Detected in (" + intersection.X + ", " + intersection.Y + ")."); // DEBUG
+                    }
+
+                    if (bodyt.yPos != null)
+                    {
+                        if (CollideSweptSegments(new LineSegment(bodyt.goal, bodyt.yPos.goal), new LineSegment(t.x, t.x + t.v), ref intersection))
+                            Testbed.PostMessage(System.Drawing.Color.Green, "LineSegments Collision Detected in (" + intersection.X + ", " + intersection.Y + ")."); // DEBUG
                     }
                 }
-            }
-            if (collisionDetected)
-            {
-                //Testbed.PostMessage(System.Drawing.Color.Green, "------------------------"); // DEBUG
             }
         }
     }
---
+++ Research/Physics/FastLatticeShapeMatching/Source/Tools/LineSegment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhysicsTestbed
+{
+    [Serializable]
+    public struct LineSegment
+    {
+        public Vector2 start;
+        public Vector2 end;
+
+        public LineSegment(Vector2 StartPos, Vector2 EndPos)
+        {
+            this.start = StartPos;
+            this.end = EndPos;
+        }
+    }
+}
